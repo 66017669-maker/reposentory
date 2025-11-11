af6acea33537929e8e4663dc821c4a71978ca8b9
66017669-maker <66017669@rbac.ac.th>
2025-11-11 12:30:09 +0700

    commit nj2

---
+++ lab3_1.js
@@ -0,0 +1,6 @@
+console.log("Hello");
+console.log("My name is Yanisa Banluesab");
+console.log("I am 20 years old");
+console.log("I am studying Computer Engineering");
+console.log("I live in Bangkok");
+
